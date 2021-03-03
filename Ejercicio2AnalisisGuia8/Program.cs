29d1a6632ec37c36893f7b32fb4455c56758e4bb
vicastillo <vicente468@outlook.com>
2021-03-03 03:16:06 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2AnalisisGuia8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2AnalisisGuia8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Double radio, angulo, x, y;
+
+
+            Console.Title = "Convertir coordenadas polares a rectangulares.";
+
+            Console.WriteLine("Ingrese el valor del radio:");
+            radio = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del ángulo:");
+            angulo = Double.Parse(Console.ReadLine());
+
+
+            x = radio * Math.Cos(angulo);
+            y = radio * Math.Sin(angulo);
+
+            Console.Write("Las coordenadas rectangulares son: ");
+            Console.Write("X = "+(x.ToString("#.##")));
+            Console.Write(" , Y = " + (y.ToString("#.##")));
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio2AnalisisGuia8/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Ejercicio2AnalisisGuia8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Ejercicio2AnalisisGuia8")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("7ef93de9-58d9-4387-957a-cca83052af10")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
